f9c1d0ec48b1480f14b6d7af94ec964bea3003fa
jhonjftb <95915251+jhonjftb@users.noreply.github.com>
2022-04-16 18:45:21 -0500

    se hicieron los cambios pertinentes en el archivo
    se hicieron los cambios necesarios para poder hacer lo necesario

--- python/main.py
+++ python/main.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     print("hola mundo")
+    print("hola a todos por fin pude hacer los cambios necesarios ")
+    print("pude funcionar todas las ramas de main en mis repositorios git")
     
