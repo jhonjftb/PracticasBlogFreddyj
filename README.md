cddf57755bd4e82815436fbee85b65e7fff814c5
jhonjftb <jtaverablandon@elite.edu.co>
2022-06-07 19:38:50 -0500

    los cambios que jtavera a realizado al proyecto, en readme.md y en python/main.py

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ es un super blog de las placticas de git y Git y GitHub de la escuela de platzi
 ahora si pude conectar todas estas ramas 
 mucho trabaja pero se pudo 
 el que persevera alcanza 
+
+
+jhon elite quiere que realice estos cambios para que se vean reflejados en el proyecto 
--- python/main.py
+++ python/main.py
@@ -5,4 +5,6 @@ if __name__ == "__main__":
     print("los nuevoss combios se deben agregar a remote ")
     print("los nuevos coambios ya estan echos")
     print("para reflejar estos cambios ")
+    print("estos cambios los realizo jtavera para jhon elite" )
+    print("el quiere que trabaje con en este importante proyecto")
     
