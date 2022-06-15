9726aa9d6c26623c634a114492a76c8e4b9cc8e8
jhonjftb <jtaverablandon@elite.edu.co>
2022-06-15 09:33:47 -0500

    cambios que realiza jtaverablandon al al proyecto clonado con permisos

--- python/main.py
+++ python/main.py
@@ -4,7 +4,8 @@ from account import Account
 if __name__ == "__main__":
     print("hola mundo")
     print("hola a todos los plazitnautas")
-    
+    print("estos son los cambios que hace jtaverablandon")
+    print("ya no hace parte del proyecto, solo pede hacer modificaciones con permiosos ")
     
     car = Car("FJB879", Account("jhohan sabedra", "AND264"))
     print(vars(car))
